fb2a0af7a973f9c4ad82ac5918a27ec49eaf8a96
sgtkaykay <60122578+sgtkaykay@users.noreply.github.com>
2020-03-18 12:31:52 +0800

    Create class.acer.php

---
+++ class.acer.php
@@ -0,0 +1,22 @@
+<?php
+ class Acer{
+	public $db;
+
+	public function __construct(){
+		$this->db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+		if(mysqli_connect_errno()){
+			echo "Database connection error.";
+			exit;
+		}
+	}
+
+	public function get_music(){
+		$sql = "SELECT * FROM tblPhone";
+		$result = mysqli_query($this->db,$sql);
+		while($r = mysqli_fetch_assoc($result)){
+			$list[] = $r;
+		}
+		return $list;
+	}
+	
+}
