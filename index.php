a298afea52369d9ae29f8aa029546f80bbe7b299
sgtkaykay <60122578+sgtkaykay@users.noreply.github.com>
2020-03-18 17:01:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+    <h3>Usage</h3>
+    <ul>
+        <li>Read all products
+            <ul><li>http://acer-api.herokuapp.com/product/read.php</li></ul>
+        </li>
+        <li>Read one product information
+            <ul><li>http://acer-api.herokuapp.com/product/read_one.php</li></ul>
+        </li>
+        <li>Display categories
+            <ul><li>http://acer-api.herokuapp.com/category/read.php</li></ul>
+        </li>
+    </ul>
+</body>
+</html>
