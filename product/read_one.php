1021447a8d43883793810f93e23448b539787d9e
sgtkaykay <60122578+sgtkaykay@users.noreply.github.com>
2020-03-18 16:49:53 +0800

    Update read_one.php

--- product/read_one.php
+++ product/read_one.php
@@ -1,15 +1,14 @@
 <?php
   $id = (isset($_GET['id']) && $_GET['id'] != '') ? $_GET['id'] : '';
-  
+
   include 'config.php';
   include 'class.acer.php';
 
   header('Content-Type: application/json');
 
   $acer = new Acer();
-  $list=$acer->get_acerone($id);
+  $list = $acer->get_acerone($id);
 
   echo "{\"Acer\":";
   echo json_encode($list);
   echo "}";
-?>
