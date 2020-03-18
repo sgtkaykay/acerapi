8817900b960798eb70bc017d80a1bf6e9619d680
sgtkaykay <60122578+sgtkaykay@users.noreply.github.com>
2020-03-18 16:47:35 +0800

    Update config.php

--- category/config.php
+++ category/config.php
@@ -1,6 +1,6 @@
 <?php
   session_start();
-  define('DB_SERVER','sql12.freesqldatabase.com');
-  define('DB_USERNAME','sql12328540');
-  define('DB_PASSWORD','AqvQkXwhts');
-  define('DB_DATABASE','sql12328540');
+  define('DB_SERVER','sql9.freesqldatabase.com');
+  define('DB_USERNAME','sql9328551');
+  define('DB_PASSWORD','EDxmcqIPA7');
+  define('DB_DATABASE','sql9328551');
