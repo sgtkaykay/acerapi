3cf37e0c57680c21a0e808b9b83ba9ee6ec91f76
sgtkaykay <60122578+sgtkaykay@users.noreply.github.com>
2020-03-18 12:53:05 +0800

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-  session_start();
-  define('DB_SERVER','sql12.freesqldatabase.com');
-  define('DB_USERNAME','sql12328540');
-  define('DB_PASSWORD','AqvQkXwhts');
-  define('DB_DATABASE','sql12328540');
